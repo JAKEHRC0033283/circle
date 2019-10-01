28c0ad2eb6bd89036f554ac1aef206217323c44c
Holloway <rc0033283@student.riversidecollege.ac.uk>
2019-10-01 12:38:42 +0100

    Add project files.

---
+++ circle/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace circle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double radius;
+            radius = double.Parse(textBox1.Text);
+
+            Double c = 2.0 * Math.PI * radius; 
+
+
+
+
+        }
+    
